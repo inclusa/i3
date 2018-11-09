4f8f53f5e70042c7e36d8a20911eaa60959e0370
Alfons Rovira <inclusa@gmail.com>
2018-11-09 15:57:12 +0100

    Initial commit

---
+++ bin/lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ 
+icon="$HOME/.config/i3/lock.png"
+img="$HOME/.cache/i3lock.png"
+ 
+scrot $img
+# Pixelate image
+convert $img -scale 10% -scale 1000% $img
+# Blur image
+#convert $img -blur 0x4 500% $img
+convert $img $icon -gravity center -composite $img
+i3lock -u -i $img
